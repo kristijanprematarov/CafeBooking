e1aa4a6989e58d980af78a5c99a670adaa39f6e8
Kristijan Prematarov <kristijanprematarov@gmail.com>
2026-03-31 14:20:45 +0200

    Update telemetry event name for menu view tracking

--- Pages/Menu.cshtml.cs
+++ Pages/Menu.cshtml.cs
@@ -28,7 +28,7 @@ public class MenuModel : PageModel
             .ThenBy(m => m.Name)
             .ToListAsync();
 
-        _telemetryClient.TrackEvent("MenuViewed", new Dictionary<string, string>
+        _telemetryClient.TrackEvent("MenuViewedV2", new Dictionary<string, string>
         {
             {"ItemCount", MenuItems.Count.ToString() }
         });
